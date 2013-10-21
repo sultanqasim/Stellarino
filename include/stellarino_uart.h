20fb8a75fe0a392ea060556e33a2210752f2ac8c
Sultan Qasim Khan <sultanqasim@gmail.com>
2013-10-20 22:48:24 -0400

    Added macro to avoid naming conflicts with stdio

--- include/stellarino_uart.h
+++ include/stellarino_uart.h
@@ -29,52 +29,51 @@ int16_t UARTgetBufferLevel(uint8_t UART);
 bool UARToverflow(uint8_t UART);
 void UARTflushReceiveBuffer(uint8_t UART);
 
+void UARTputc(uint8_t UART, char c);
+char UARTgetc(uint8_t UART);
+int UARTpeek(uint8_t UART);
+char UARTpeekBlocking(uint8_t UART);
+
+void UARTputln(uint8_t UART);
+
 // Will not append newline automatically
-#define puts(s) UARTputs(0, (s))
 void UARTputs(uint8_t UART, const char * str);
 
 // Reads until newline or num chars
-#define gets(s, n) UARTgets(0, (s), (n))
 char *UARTgets(uint8_t UART, char * str, int num);
 
-#define putc(c) UARTputc(0, (c))
-void UARTputc(uint8_t UART, char c);
-
-#define putln() UARTputln(0)
-void UARTputln(uint8_t UART);
-
-#define getc() UARTgetc(0)
-char UARTgetc(uint8_t UART);
-
-// These return -255 if buffers are empty
-#define peek() UARTpeek(0)
-int UARTpeek(uint8_t UART);
-
-#define peekBlocking() UARTpeekBlocking(0)
-char UARTpeekBlocking(uint8_t UART);
-
-#define puti(i) UARTputi(0, (i))
 void UARTputi(uint8_t UART, long i);
-
-#define geti() UARTgeti(0)
 long UARTgeti(uint8_t UART);
 
-#define putu(u, d) UARTputu(0, (u), (d))
 void UARTputu(uint8_t UART, unsigned long u, uint8_t digits);
-
-#define getu(d) UARTgetu(0, (d))
 unsigned long UARTgetu(uint8_t UART, uint8_t digits);
 
-#define puth(h, d) UARTputh(0, (h), (d))
 void UARTputh(uint8_t UART, unsigned long h, uint8_t digits);
-
-#define geth(d) UARTgeth(0, (d))
 unsigned long UARTgeth(uint8_t UART, uint8_t digits);
 
-#define putf(f, d) UARTputf(0, (f), (d))
 void UARTputf(uint8_t UART, float f, uint8_t decimal);
-
-#define getf() UARTgetf(0)
 float UARTgetf(uint8_t UART);
+
+// Some of these macros have names that conflict with stdio.h
+// You can #define PERMIT_STDIO to avoid the conflict
+#ifndef PERMIT_STDIO
+
+#define puts(s) UARTputs(0, (s))
+#define gets(s, n) UARTgets(0, (s), (n))
+#define putc(c) UARTputc(0, (c))
+#define putln() UARTputln(0)
+#define getc() UARTgetc(0)
+#define peek() UARTpeek(0)
+#define peekBlocking() UARTpeekBlocking(0)
+#define puti(i) UARTputi(0, (i))
+#define geti() UARTgeti(0)
+#define putu(u, d) UARTputu(0, (u), (d))
+#define getu(d) UARTgetu(0, (d))
+#define puth(h, d) UARTputh(0, (h), (d))
+#define geth(d) UARTgeth(0, (d))
+#define putf(f, d) UARTputf(0, (f), (d))
+#define getf() UARTgetf(0)
+
+#endif
 
 #endif
